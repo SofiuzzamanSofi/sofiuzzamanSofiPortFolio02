80f3e516b4a9e5f1ca2017f3a16ea846722cf7b4
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-12-13 16:22:14 +0600

    send email feedback success from emailjs

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
-import contactMe from '../assets/contactMe.gif';
+import emailjs from '@emailjs/browser';
 
 const ContactMe = () => {
 
+    const form = useRef();
     const [loading, setLoading] = useState();
     // const handleSubmit = () => {
     //     toast.success("kam ok")
@@ -13,19 +14,30 @@ const ContactMe = () => {
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => {
-        toast.success("kam ok")
-        toast.error("kam hoi nai")
-        console.log(data);
+        // console.log(data);
+        // console.log(errors);
     }
-    console.log(errors);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log(process.env.REACT_APP_SERVICE_ID);
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
+            .then((result) => {
+                console.log(result.text);
+                toast.success(`Dear ${e.target?.name.value}, Your Valuable Feedback/Message is Submitted.`)
+            }, (error) => {
+                console.log(error.text);
+                toast.error(`Dear ${e.target?.name.value}, Something error.`)
+            });
+    };
 
 
     return (
-        <div className={`py-10 bg-${contactMe}`}>
+        <div className="py-10">
             <div className="flex flex-col md:flex-row md justify-between gap-5">
                 <div className='w-full'>
                     <div className="text-center lg:text-left grid gap-5">
-                        <p className='text-2xl md:text-5xl'>Contact Me <span className='text-primary'>...</span></p>
+                        <p className='text-3xl md:text-5xl'>Contact Me <span className='text-primary'>...</span></p>
                         <p>If you have any query or suggestion or any other pls tell me in detail without any hesitate .</p>
                     </div>
                 </div>
@@ -33,13 +45,14 @@ const ContactMe = () => {
                     <img src={contactMe} alt="" className="w-full sm:w-96 rounded-md" />
                 </div> */}
                 <div className="w-full">
-                    <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* <form onSubmit={handleSubmit(onSubmit)} ref={form} onSubmit={sendEmail}> */}
+                    <form ref={form} onSubmit={sendEmail}>
                         <div className='flex flex-col sm:flex-row gap-5 '>
                             <div className="form-control w-full">
                                 <label className="label">
                                     <span className="label-text">Name</span>
                                 </label>
-                                <input type="text" placeholder="Your Name" className="input input-bordered"
+                                <input type="text" name="name" required placeholder="Your Name" className="input input-bordered"
                                     {...register("name", { required: true, minLength: 6 })} />
                                 <p className='text-red-800'>{errors?.name ? "At least 6 character" : ""}</p>
                             </div>
@@ -47,7 +60,7 @@ const ContactMe = () => {
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
-                                <input type="email" placeholder="example@mail.com" className="input input-bordered"
+                                <input type="email" name="email" required placeholder="example@mail.com" className="input input-bordered"
                                     {...register("email", { required: "Email is required" })}
                                 />
                                 <p className='text-red-800'>{errors?.email ? errors?.email?.message : ""}</p>
@@ -57,13 +70,13 @@ const ContactMe = () => {
                             <label className="label">
                                 <span className="label-text">Message</span>
                             </label>
-                            <textarea name="message" id="" cols="30" rows="10" className="input h-28"
+                            <textarea name="message" required id="" cols="30" rows="10" className="input h-28"
                                 {...register("message", { required: true, maxLength: 20 })}
                             />
                             <p className='text-red-800'>{errors?.message ? "Feedback/Message at least 20 character" : ""}</p>
                         </div>
                         <div className="form-control mt-6">
-                            <button type='submit' className="btn btn-primary">Submit</button>
+                            <input type="submit" value="Submit" className="input input-bordered bg-primary cursor-pointer" />
                         </div>
                     </form>
                 </div>
--- src/components/pages/Service.js
+++ src/components/pages/Service.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Service = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log("current", form.current);
+        emailjs.sendForm('service_y24v1fq', 'template_q4g2usf', form.current, 'rH_rcDzmrZ-Ro-Udd')
+            .then((result) => {
+                console.log(result.text);
+                console.log("send succsess");
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
-        <div>
-            service .js
-        </div>
+        <form ref={form} onSubmit={sendEmail} className="block">
+            <label>Name</label>
+            <input type="text" name="name" />
+            <label>Email</label>
+            <input type="email" name="email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
+        </form>
     );
 };
 
