b64402fecb1d693ebc4c90e233907dc6624b000f
SofiuzzamanSofi <sofiuzzaman.sf@gmail.com>
2022-12-22 01:39:45 +0600

    change navbar mobile menu items

--- src/components/pages/Header.js
+++ src/components/pages/Header.js
@@ -22,6 +22,19 @@ const Header = () => {
         });
 
 
+    // Navbar Items --------
+    const navbarItmes = <>
+        <a href={location.pathname !== "/" ? "/" : "#aboutMe"}>About Me</a>
+        <a href={location.pathname !== "/" ? "/" : "#contact"}>Contact Me</a>
+        <a href={location.pathname !== "/" ? "/" : "#projects"}>Projects</a>
+        <Link to="/blogs">My Blogs</Link>
+        <Link to={cv} target="_blank" download
+            className="py-4 px-9 rounded-md border-[3px] border-primary hover:bg-primary hover:text-white"
+        >
+            Resume
+        </Link>
+    </>
+
     return (
         <header className=" bg-slate-50 text-black dark:bg-black dark:text-white">
             <div className="px-7 py-2 container flex items-center justify-between max-w-screen-2xl mx-auto border dark:border-gray-800">
@@ -30,23 +43,17 @@ const Header = () => {
                         <img src={sofiLogo} alt="Logo" className="border-primary border-2 bg-primary hover:bg-[#4800ff] px-6 py-[2px] w-40 md:w-64 rounded-md " />
                     </Link>
                 </div>
-                <nav className=" hidden text-sm lg:flex items-center justify-between gap-5">
-                    {/* <Link to="/about">About Me</Link> */}
-                    <a href={location.pathname !== "/" ? "/" : "#aboutMe"}>About Me</a>
-                    <a href={location.pathname !== "/" ? "/" : "#contact"}>Contact Me</a>
-                    <a href={location.pathname !== "/" ? "/" : "#projects"}>Projects</a>
-                    <Link to="/blogs">My Blogs</Link>
-                    <Link to={cv} target="_blank" download
-                        className="py-4 px-9 rounded-md border-[3px] border-primary hover:bg-primary hover:text-white"
-                    >
-                        Resume
-                    </Link>
+                <nav className=" hidden text-sm md:flex items-center justify-between gap-5">
+                    {navbarItmes}
                 </nav>
-                <button className="lg:hidden" onClick={() => setShowNav(!showNav)}>
+                <button className="md:hidden" onClick={() => setShowNav(!showNav)}>
                     <HiOutlineMenuAlt3 size={28} />
                 </button>
+
+
+                {/* choto (mobile) devices jonno--------- */}
                 <nav
-                    className={` lg:hidden text-base w-full md:w-2/5 bg-slate-300 dark:bg-slate-900 h-full top-0 right-0 z-10 flex-col flex items-center justify-center  gap-5 fixed ${showNav ? "translate-x-0" : "translate-x-full"
+                    className={` md:hidden text-base w-full md:w-2/5 bg-slate-300 dark:bg-slate-900 h-full top-0 right-0 z-10 flex-col flex items-center justify-center  gap-5 fixed ${showNav ? "translate-x-0" : "translate-x-full"
                         } duration-300 transition-all`}
                 >
                     <button
@@ -55,17 +62,14 @@ const Header = () => {
                     >
                         <FaTimes size="32" />
                     </button>
+                    {navbarItmes}
                     {/* <Link to="/about">About Me</Link> */}
-                    <a href={location.pathname !== "/" ? "/" : "#aboutMe"}>About Me</a>
-                    <a href={location.pathname !== "/" ? "/" : "#contact"}>Contact Me</a>
-                    <a href={location.pathname !== "/" ? "/" : "#projects"}>Projects</a>
-                    <Link to="/blogs">My Blogs</Link>
-                    <Link
+                    {/* <Link
                         to={cv} target="_blank" download
                         className="bg-red-600 py-2  px-3 rounded"
                     >
                         Resume
-                    </Link>
+                    </Link> */}
                 </nav>
             </div>
         </header>
